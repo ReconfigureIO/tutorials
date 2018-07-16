2e3efa639b64231c88a2232acab2eb4a6646da73
Rosie Yohannan <rosie.yohannan@reconfigure.io>
2018-07-16 22:34:51 +0100

    improve comments to make things clearer

--- multiply-array/cmd/bench-multiply-10/main.go
+++ multiply-array/cmd/bench-multiply-10/main.go
@@ -13,30 +13,24 @@ func main() {
 	state := NewState(10)
 	defer state.Release()
 
-	log.Println()
-	log.Println()
-	log.Printf("Benchmark round:")
-
 	result := testing.Benchmark(state.Run)
+
 	log.Println()
 	log.Println()
-	log.Printf("Benchmark:")
+	log.Printf("Time taken to collect and process an array of 10 integers:")
+	log.Println()
+
 	fmt.Println(result)
+
+	log.Println()
 }
 
-//func BenchmarkMultiply(b *testing.B) {
-//  state := NewState(100)
-//  defer state.Release()
-
-//  b.Run("100_inputs", state.Run)
-//}
-
 type State struct {
-	// Kernel, InputBuf, OutputBuf, input, result.
+	// Everything that needs setting up - kernel, input buffer, output buffer, input var, result var.
 	world      xcl.World
 	program    *xcl.Program
 	krnl       *xcl.Kernel
-	buff       *xcl.Memory
+	inputBuff  *xcl.Memory
 	outputBuff *xcl.Memory
 	input      []uint32
 	output     []uint32
@@ -48,18 +42,16 @@ func NewState(nInputs int) *State {
 	size := uint(nInputs) * 4    // number of bytes needed to hold the input and output data
 
 	s := &State{
-		world:      w,
-		program:    p,
-		krnl:       p.GetKernel("reconfigure_io_sdaccel_builder_stub_0_1"),
-		buff:       w.Malloc(xcl.ReadOnly, size),  // constructed as a function of nInputs
-		outputBuff: w.Malloc(xcl.ReadWrite, size), // output will be the same size as input
-		input:      make([]uint32, nInputs),       //variable to store input data
-		output:     make([]uint32, nInputs),       // variable to store result data
+		world:      w,                                                      // allocate a new world for interacting with the FPGA
+		program:    p,                                                      // Import the compiled code that will be loaded onto the FPGA (referred to here as a kernel)
+		krnl:       p.GetKernel("reconfigure_io_sdaccel_builder_stub_0_1"), // Right now these two identifiers are hard coded as an output from the build process
+		inputBuff:  w.Malloc(xcl.ReadOnly, size),                           // constructed an input buffer as a function of nInputs
+		outputBuff: w.Malloc(xcl.ReadWrite, size),                          // In this example our output will be the same size as our input
+		input:      make([]uint32, nInputs),                                // make a variable to store our input data
+		output:     make([]uint32, nInputs),                                // make a variable to store our results data
 	}
 
 	// Seed the input array with incrementing values
-	//max := int(nInputs)
-
 	for i, _ := range s.input {
 		s.input[i] = uint32(i)
 	}
@@ -67,40 +59,35 @@ func NewState(nInputs int) *State {
 	// To avoid measuring warmup cost of the first few calls (especially in sim)
 	const warmup = 2
 	for i := 0; i < warmup; i++ {
-		log.Println()
-		log.Println()
-		log.Printf("Warm up round:")
 		s.feedFPGA()
 	}
 
 	return s
 }
 
+// This function will calculate the benchmark, it will run repeatedly until it achieves a reliable result
 func (s *State) Run(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		s.feedFPGA()
 	}
 }
 
+// This function frees up buffers and released the World an program used to interact with the FPGA
 func (s *State) Release() {
-	s.buff.Free()
+	s.inputBuff.Free()
 	s.outputBuff.Free()
 	s.program.Release()
 	s.world.Release()
 }
 
+// This function writes our sample data to memory, tells the FPGA where it is, and where to put the result and starts the FPGA runnings
 func (s *State) feedFPGA() {
 	// write input to memory
-	binary.Write(s.buff.Writer(), binary.LittleEndian, &s.input)
-	// Zero out the space for the result
-	//binary.Write(s.outputBuff.Writer(), binary.LittleEndian, &s.output)
+	binary.Write(s.inputBuff.Writer(), binary.LittleEndian, &s.input)
 
-	// Send the location of the input array as the first argument
-	s.krnl.SetMemoryArg(0, s.buff)
-	// Send the location the FPGA should put the result as the second argument
-	s.krnl.SetMemoryArg(1, s.outputBuff)
-	// Send the length of the input array as the third argument, so the FPGA knows what to expect
-	s.krnl.SetArg(2, uint32(len(s.input)))
+	s.krnl.SetMemoryArg(0, s.inputBuff)    // Send the location of the input data as the first argument
+	s.krnl.SetMemoryArg(1, s.outputBuff)   // Send the location the FPGA should put the result as the second argument
+	s.krnl.SetArg(2, uint32(len(s.input))) // Send the length of the input array as the third argument, so the FPGA knows what to expect
 
 	// start the FPGA running
 	s.krnl.Run(1, 1, 1)
@@ -108,6 +95,6 @@ func (s *State) feedFPGA() {
 	// Read the results into our output variable
 	binary.Read(s.outputBuff.Reader(), binary.LittleEndian, &s.output)
 
-	log.Printf("Input: %v ", s.input)
-	log.Printf("Output: %v ", s.output)
+	//log.Printf("Input: %v ", s.input)
+	//log.Printf("Output: %v ", s.output)
 }
